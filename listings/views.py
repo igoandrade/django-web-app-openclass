5fc7f42a40fb5a3968f4512d687332f14fe4551e
Laboratório de Física - UFT <labfis.cup@uft.edu.br>
2022-11-22 17:09:22 -0300

    Modelos em Django

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from listings.models import Band
 
 # Create your views here.
+def hello(request):
+    bands = Band.objects.all()
+    return HttpResponse('<h1>Olá Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>Sobre nós</h1><p>Somos uma loja online ...</p>')
