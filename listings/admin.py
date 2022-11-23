53b7f555f582f90d40a8e8911146620f9de55ba9
Laboratório de Física - UFT <labfis.cup@uft.edu.br>
2022-11-23 08:49:36 -0300

    Criação do modelo de banco de dados e do superusuário

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from listings.models import Band
+
+class BandAdmin(admin.ModelAdmin):
+    list_display = ('name', 'year_formed', 'genre')
 
 # Register your models here.
+admin.site.register(Band, BandAdmin)
--- listings/views.py
+++ listings/views.py
@@ -5,7 +5,7 @@ from listings.models import Band
 # Create your views here.
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse('<h1>Olá Django!</h1>')
+    return render(request, 'listings/hello.html', {'bands': bands})
 
 def about(request):
     return HttpResponse('<h1>Sobre nós</h1><p>Somos uma loja online ...</p>')
