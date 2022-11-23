8c56517325c72c8f1b058405a85cba3137d419a8
Laboratório de Física - UFT <labfis.cup@uft.edu.br>
2022-11-23 09:37:14 -0300

    Relacionamentos many-to-many

--- listings/models.py
+++ listings/models.py
@@ -10,7 +10,7 @@ class Band(models.Model):
 
     name = models.fields.CharField(max_length=100)
     genre = models.fields.CharField(choices=Genre.choices, max_length=5)
-    biography = models.fields.CharField(default='', max_length=1000)
+    biography = models.fields.TextField(default='')
     year_formed = models.fields.IntegerField(
         validators=[MinValueValidator(1900), MaxValueValidator(2022)],
         default=2000
@@ -20,3 +20,22 @@ class Band(models.Model):
 
     def __str__(self):
         return f"{self.name}"
+    
+class Listing(models.Model):
+    class Type(models.TextChoices):
+        RECORDS = 'R'
+        CLOTHONG = 'C'
+        MISCELLANEOUS = 'M'
+
+    title = models.fields.CharField(max_length=100)
+    description = models.fields.TextField(default='')
+    year = models.fields.IntegerField(
+        validators=[MinValueValidator(1900), MaxValueValidator(2022)],
+        default=2000
+    )
+    sold = models.fields.BooleanField(default=False)
+    type = models.fields.CharField(choices=Type.choices, max_length=50)
+    band = models.ForeignKey(Band, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f"{self.title}"
